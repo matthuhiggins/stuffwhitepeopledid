cceb4a709c048e32970ef2001005e949b3850372
Matthew <developer@matthewhiggins.com>
2010-05-17 17:35:39 -0700

    now its over

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -8,4 +8,9 @@ class PostTest < ActiveSupport::TestCase
   def test_count
     assert_equal Post.all.count, Post.count
   end
+  
+  def test_find
+    assert_equal 43, Post.find(43).number
+    assert_nil Post.find(9999)
+  end
 end

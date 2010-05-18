e9e650f6ebc67f18e242794f86fc1f3961649441
Matthew <developer@matthewhiggins.com>
2010-05-18 16:32:08 -0700

    moar

--- app/helpers/facebook_helper.rb
+++ app/helpers/facebook_helper.rb
@@ -1,5 +1,10 @@
 module FacebookHelper
   def facebook_picture(uid)
-    image_tag "http://graph.facebook.com/#{uid}/picture"
+    content_tag 'fb:profile-pic', '', :uid => uid, :linked => false
+    # image_tag "http://graph.facebook.com/#{uid}/picture"
+  end
+
+  def facebook_name(uid)
+    content_tag 'fb:name', '', :uid => uid, :linked => false
   end
 end

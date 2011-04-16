6be850436b82410db1d8c871d20fc152cdbf9768
Matthew Higgins <developer@matthewhiggins.com>
2011-04-15 19:15:29 -0700

    more better

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 StuffWhitePeopleDid::Application.routes.draw do
-  root :to => "home#index"
+  root to: 'home#index'
+  match 'activity' => 'home#activity', as: :activity
+  
   match 'me/:action' => 'home'
   resources :accomplishments, :only => [:update, :destroy]
 
